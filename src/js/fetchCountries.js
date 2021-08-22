25c5373d311f6baf437872cd0608578a5b6e9f14
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-08-22 11:22:52 +0200

    (async/await)

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,14 +2,26 @@
 // console.log('this is - fetchCountries.js');
 const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
 
+// old version
 
-function fetchCountries(name) {
-const PROPERTIES = 'prop=name;capital;population;flag;languages'
-    return fetch(`${BASE_URL}${name}?${PROPERTIES}`).then(response => {
-        return response.json();
-    })
-    .catch(error => {console.log(error)
-    });
+// function fetchCountries(name) {
+// const PROPERTIES = 'prop=name;capital;population;flag;languages'
+//     return fetch(`${BASE_URL}${name}?${PROPERTIES}`).then(response => {
+//         return response.json();
+//     })
+//     .catch(error => {console.log(error)
+//     });
+// }
+
+async function fetchCountries(name) {
+    const PROPERTIES = 'prop=name;capital;population;flag;languages'
+    try {
+        const response = await fetch(`${BASE_URL}${name}?${PROPERTIES}`);
+        const country = await response.json();
+        return country;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export default { fetchCountries }
--- src/js/inputCountries.js
+++ src/js/inputCountries.js
@@ -17,28 +17,61 @@ const countryListRef = document.querySelector('.country-list');
 
 inputRef.addEventListener('input', debounce(onInputCountry,DEBOUNCE_DELAY));
 
-function onInputCountry(evt) {
+async function onInputCountry(evt) {
 
     const countryName = evt.target.value.trim();
-    // console.log(countryName);
-    if (countryName ==='') {
+        if (countryName ==='') {
         return Notiflix.Notify.failure('Please enter country');
-    }
-   
-    FC.fetchCountries(countryName).then(countryName => { 
-       clearPage()
-        if (countryName.status === 404) {
-            Notiflix.Notify.failure('Oops, there is no country with that name');
-        } else if (countryName.length > 10) {
-            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        } else if (countryName.length === 1) {
-            getMarkupOfOneCountry(countryName);
-        } else if (countryName.length > 1 && countryName.length <= 10) {
-            getMarkupOfCountries(countryName);
         }
-    });
+    
+    try {
+       const countryNameResponse = await FC.fetchCountries(countryName);
+        
+        clearPage();
 
+        if (countryNameResponse.status === 404) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        } else if (countryNameResponse.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+            // || countryNameResponse.length === 2 додав щоб малювалась країна якщо відповідь 2
+        else if (countryNameResponse.length === 1 || countryNameResponse.length === 2) {
+            getMarkupOfOneCountry(countryNameResponse);
+        }
+        else if (countryNameResponse.length > 1 && countryNameResponse.length <= 10) {
+            getMarkupOfCountries(countryNameResponse);
+        }
+        
+   } catch (error) {
+       console.log(error);
+    }
+    
 }
+
+// стара версія
+
+// function onInputCountry(evt) {
+
+//     const countryName = evt.target.value.trim();
+//     // console.log(countryName);
+//     if (countryName ==='') {
+//         return Notiflix.Notify.failure('Please enter country');
+//     }
+   
+//     FC.fetchCountries(countryName).then(countryName => { 
+//        clearPage()
+//         if (countryName.status === 404) {
+//             Notiflix.Notify.failure('Oops, there is no country with that name');
+//         } else if (countryName.length > 10) {
+//             Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+//         } else if (countryName.length === 1) {
+//             getMarkupOfOneCountry(countryName);
+//         } else if (countryName.length > 1 && countryName.length <= 10) {
+//             getMarkupOfCountries(countryName);
+//         }
+//     });
+
+// }
 
 function getMarkupOfCountries(countries) {
     countryListRef.insertAdjacentHTML('beforeend', markupOfCountries(countries));
